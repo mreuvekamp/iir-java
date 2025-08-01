77213e7d07728fa6521ed72a1284237720dd6c62
Mark Reuvekamp <mark.reuvekamp@rijksoverheid.nl>
2025-08-01 15:35:05 +0200

    Voorbereiding opdracht 1

--- Main.java
+++ Main.java
@@ -8,52 +8,41 @@ class Main {
     public static void main(String[] args)
     {
         // Stores two numbers
-        double num1, num2;
+        double number1, number2;
 
         // Take input from the user
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter the numbers:");
 
         // Take the inputs
-        num1 = sc.nextDouble();
-        num2 = sc.nextDouble();
+        number1 = scanner.nextDouble();
 
         System.out.println("Enter the operator (+,-,*,/):");
 
-        char op = sc.next().charAt(0);
-        double o = 0;
+        char operator = scanner.next().charAt(0);
+        double result = 0;
 
-        switch (op) {
+        switch (operator) {
             // case to add two numbers
             case '+':
-                o = num1 + num2;
-                break;
-
-            // case to subtract two numbers
-            case '-':
-                o = num1 - num2;
-                break;
-
-            // case to multiply two numbers
-            case '*':
-                o = num1 * num2;
+                result = number1 + number2;
                 break;
 
             // case to divide two numbers
             case '/':
-                o = num1 / num2;
+                result = number1 / number2;
                 break;
 
             default:
-                System.out.println("You enter wrong input");
+                System.out.println("Unknown operator");
         }
 
         System.out.println("The final result:");
         System.out.println();
 
         // print the final result
-        System.out.println(num1 + " " + op + " " + num2
-            + " = " + o);
+        System.out.println(number1 + " " + operator + " " + number2
+            + " = " + result);
     }
 }
